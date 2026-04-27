9af7eb411b512b06b42c19411745c05dfcd709f3
Shantanoo-Chandorkar <chandorkarshantanoo@gmail.com>
2026-04-28 05:29:14 +0530

    Fixed color issues in the CharNode component

--- components/CharNode.jsx
+++ components/CharNode.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const CharNode = React.memo(({ char, status }) => {
-    let colorClass = 'text-gray-300';
+    let colorClass = 'text-gray-400';
 
     if (status === 'correct') {
         colorClass = 'text-green-500';
     } else if (status === 'incorrect') {
         // For spaces - background.
         // For text - text color.
-        colorClass = char === ' ' ? 'text-red-400/50' : 'text-red-500';
+        colorClass = char === ' ' ? 'bg-red-400/50' : 'text-red-500';
     }
 
     return <span className={`text-2xl font-mono ${colorClass}`}>{char}</span>;
