2cb7c5dd9647e9d85d39942ee7a89c09ebd1ce98
Shantanoo-Chandorkar <chandorkarshantanoo@gmail.com>
2026-04-28 03:14:35 +0530

    Added timer and difficulty states and select elements for the same

--- app/page.js
+++ app/page.js
@@ -2,27 +2,83 @@
 import { useState, useEffect } from 'react';
 import data from '../data/typing_quotes_categorized.json';
 
-const getRandomQuote = () => {
-    const quoteKeys = Object.keys(data);
+const getRandomQuote = (difficulty) => {
+    // const quoteKeys = Object.keys(data);
 
-    const randomQuoteKey = quoteKeys[Math.floor(Math.random() * quoteKeys.length)];
-    const randomQuote = data[randomQuoteKey][Math.floor(Math.random() * data[randomQuoteKey].length)];
+    // const randomQuoteKey = quoteKeys[Math.floor(Math.random() * quoteKeys.length)];
+    const randomQuote = data[difficulty][Math.floor(Math.random() * data[difficulty].length)];
 
     return randomQuote;
-}
+};
+
+const TIMER_VALUES = [15, 30, 60, 120];
+const NUMBER_OF_WORDS = [10, 25, 50, 100];
+const DIFFICULTY_LEVELS = ['easy', 'medium', 'hard'];
 
 export default function Home() {
     const [randomQuote, setRandomeQuote] = useState(null);
+    const [timer, setTimer] = useState(15);
+    const [difficulty, setDifficulty] = useState('easy');
 
     useEffect(() => {
-        setRandomeQuote(getRandomQuote());
-    }, []);
+        setRandomeQuote(getRandomQuote(difficulty));
+    }, [difficulty]);
+
+    const handleTimerChange = (e) => {
+        setTimer(e.target.value);
+    };
+
+    const handleDifficultyChange = (e) => {
+        setDifficulty(e.target.value);
+    };
     return (
         <div className="flex w-full m-auto justify-center items-center h-screen flex-col gap-4">
             <h1>{`Type Forge - Let's build this in a non-AI way.`}</h1>
-            <div className='w-[80%] mx-auto flex flex-col gap-2 justify-center items-center'>
-                <label htmlFor='type-input' className='w-full text-left'>Type here</label>
-                <textarea className='w-full p-10 border border-gray-300 border-2' id='type-input' placeholder={randomQuote?.quote}></textarea>
+            <div className="w-[80%] mx-auto flex flex-col gap-2 justify-center items-start">
+                <div className="w-full flex gap-8">
+                    <>
+                        <label className="flex gap-2" htmlFor="timer">
+                            Timer
+                            <select
+                                className="border border-gray-300 border-2"
+                                onChange={handleTimerChange}
+                                value={timer}
+                                id="timer"
+                            >
+                                <option value="">Select the timer value</option>
+                                {TIMER_VALUES.map((timer, index) => (
+                                    <option key={index} value={timer}>
+                                        {timer}
+                                    </option>
+                                ))}
+                            </select>
+                        </label>
+                    </>
+                    <>
+                        <label className="flex gap-2" htmlFor="difficulty">
+                            Difficulty
+                            <select
+                                id="difficulty"
+                                className="border border-gray-300 border-2"
+                                onChange={handleDifficultyChange}
+                            >
+                                {/* <option value=''>Select the difficulty value</option> */}
+                                {DIFFICULTY_LEVELS.map((difficulty, index) => (
+                                    <option key={index} value={difficulty}>
+                                        {difficulty}
+                                    </option>
+                                ))}
+                            </select>
+                        </label>
+                    </>
+                </div>
+                <label htmlFor="type-input" className="w-full text-left">
+                    <textarea
+                        className="w-full p-10 border border-gray-300 border-2"
+                        id="type-input"
+                        placeholder={randomQuote?.quote}
+                    ></textarea>
+                </label>
             </div>
         </div>
     );
